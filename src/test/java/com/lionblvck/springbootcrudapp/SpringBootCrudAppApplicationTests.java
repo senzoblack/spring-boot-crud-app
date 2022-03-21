137f710b371be844fd0ad97eabf21f86530cefee
Senzo <senzo.mthembu@playsafesa.com>
2022-03-21 14:52:02 +0200

    Springboot starter template

---
+++ src/test/java/com/lionblvck/springbootcrudapp/SpringBootCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lionblvck.springbootcrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
