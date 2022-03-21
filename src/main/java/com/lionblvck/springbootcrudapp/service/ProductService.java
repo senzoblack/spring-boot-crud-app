e15942ac1818dde5807c3dd6e8061b81f083ee26
Senzo <senzo.mthembu@playsafesa.com>
2022-03-21 21:13:56 +0200

    Latest changes

--- src/main/java/com/lionblvck/springbootcrudapp/service/ProductService.java
+++ src/main/java/com/lionblvck/springbootcrudapp/service/ProductService.java
@@ -1,2 +1,46 @@
-package com.lionblvck.springbootcrudapp.service;public class ProductService {
+package com.lionblvck.springbootcrudapp.service;
+
+import com.lionblvck.springbootcrudapp.entity.Product;
+import com.lionblvck.springbootcrudapp.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository repository;
+
+    //POST Methods
+    public Product saveProduct(Product product){
+        return repository.save(product);
+    }
+    public List<Product> saveProducts(List<Product> products){
+        return repository.saveAll(products);
+    }
+    //GET Methods
+    //Fetch all list of products
+    public List<Product> getProducts(){
+        return repository.findAll();
+    }
+    //Get single product by ID
+    public Product getProductById(int id){
+        return repository.findById(id).orElse(null);
+    }
+    public Product getProductByName(String name){
+        return repository.findByName(name);
+    }
+    public String deleteProduct(int id){
+        repository.deleteById(id);
+        return "product removed"+id;
+    }
+    //Update existing products by Name, Quantity & Price
+    public Product updateProduct(Product product){
+        Product existingProduct = repository.findById(product.getId()).orElse(null);
+        existingProduct.setName(product.getName());
+        existingProduct.setQuantity(product.getQuantity());
+        existingProduct.setPrice(product.getPrice());
+        return repository.save(existingProduct);
+    }
 }
